4c1509c2257b30acd968887366c949dc4fb81ad6
Kartik Gaur <99113700+Krunch1e@users.noreply.github.com>
2024-07-07 13:02:07 +0530

    Add files via upload

--- ValorantAgentLocker.py
+++ ValorantAgentLocker.py
@@ -39,12 +39,23 @@ yoru		=	(1044,1014)
 
 all_agents = [astra, breach, brimstone, chamber, clove, cypher, deadlock, fade, gekko, harbor, iso, jett, kayo, killjoy, neon, omen, phoenix, raze, reyna, sage, skye, sova, viper, yoru]
 
-
 current_window = None
 
 def switch_window(window_name):
     global current_window
     current_window = window_name
+
+
+def In_Lock(Character):
+    running = True
+    sleep(3)
+    while running:
+        pag.leftClick(Character)
+        pag.leftClick(lock_in)
+        sleep(0.01)
+        
+        if kb.is_pressed('f6'):
+            running = False
 
 
 def clear_screen():
@@ -53,21 +64,17 @@ def clear_screen():
 
 
 def randomize_agent():
-    # Wait for F6 key press to start
-    while not kb.is_pressed('f6'):
-        sleep(0.0001)  # Short delay to avoid overwhelming CPU
-
-    # Random selection loop
-    while True:
+    running = True
+    sleep(3)
+    while running:
         selected_agent = random.choice(all_agents)
-        pag.moveTo(selected_agent)
+        pag.leftClick(selected_agent)
 
-        # Check if F1 is pressed to lock in
-        if kb.is_pressed('f6'):
-            pag.moveTo(lock_in)
-            break
-
-
+        while kb.is_pressed('f6'):
+            pag.leftClick(lock_in)
+            running = False
+        
+       
 
 def randomizer_screen():
     clear_screen()
@@ -85,8 +92,200 @@ def agent_instalocker_screen():
     clear_screen()
     switch_window('Insta-Locker')
     Home = tk.Button(root, text= "Home", command= main_window, height= 3, width= 10, bg='green', fg='white')
-    Home.pack()
+    Home.grid(row= 0, column= 4, pady= 50)
 
+    Astra = tk.Button(root,
+                      text="Astra",
+                      height= 3,
+                      width= 8,
+                      command=lambda: In_Lock(astra))
+    
+    Breach = tk.Button(root,
+                      text="Breach",
+                      height= 3,
+                      width= 8,
+                      command=lambda: In_Lock(breach))
+    
+    Brimstone = tk.Button(root,
+                      text="Brimstone",
+                      height= 3,
+                      width= 8,
+                      command=lambda: In_Lock(brimstone))
+    
+    Chamber = tk.Button(root,
+                      text="Chamber",
+                      height= 3,
+                      width= 8,
+                      command=lambda: In_Lock(chamber))
+    
+    Clove = tk.Button(root,
+                      text="Clove",
+                      height= 3,
+                      width= 8,
+                      command=lambda: In_Lock(clove))
+    
+    Cypher = tk.Button(root,
+                      text="Cypher",
+                      height= 3,
+                      width= 8,
+                      command=lambda: In_Lock(cypher))
+    
+    Deadlock = tk.Button(root,
+                      text="Deadlock",
+                      height= 3,
+                      width= 8,
+                      command=lambda: In_Lock(deadlock))
+    
+    Fade = tk.Button(root,
+                      text="Fade",
+                      height= 3,
+                      width= 8,
+                      command=lambda: In_Lock(fade))
+    
+    Gekko = tk.Button(root,
+                      text="Gekko",
+                      height= 3,
+                      width= 8,
+                      command=lambda: In_Lock(gekko))
+    
+    Harbor = tk.Button(root,
+                      text="Harbor",
+                      height= 3,
+                      width= 8,
+                      command=lambda: In_Lock(harbor))
+    
+    Iso = tk.Button(root,
+                      text="Iso",
+                      height= 3,
+                      width= 8,
+                      command=lambda: In_Lock(iso))
+    
+    Jett = tk.Button(root,
+                      text="Jett",
+                      height= 3,
+                      width= 8,
+                      command=lambda: In_Lock(jett))
+    
+    Kayo = tk.Button(root,
+                      text="KAY/O",
+                      height= 3,
+                      width= 8,
+                      command=lambda: In_Lock(kayo))
+    
+    Killjoy = tk.Button(root,
+                      text="Killjoy",
+                      height= 3,
+                      width= 8,
+                      command=lambda: In_Lock(killjoy))
+    
+    Neon = tk.Button(root,
+                      text="Neon",
+                      height= 3,
+                      width= 8,
+                      command=lambda: In_Lock(neon))
+    
+    Omen = tk.Button(root,
+                      text="Omen",
+                      height= 3,
+                      width= 8,
+                      command=lambda: In_Lock(omen))
+    
+    Phoenix = tk.Button(root,
+                      text="Phoenix",
+                      height= 3,
+                      width= 8,
+                      command=lambda: In_Lock(phoenix))
+    
+    Raze = tk.Button(root,
+                      text="Raze",
+                      height= 3,
+                      width= 8,
+                      command=lambda: In_Lock(raze))
+    
+    Reyna = tk.Button(root,
+                      text="Reyna",
+                      height= 3,
+                      width= 8,
+                      command=lambda: In_Lock(reyna))
+    
+    Sage = tk.Button(root,
+                      text="Sage",
+                      height= 3,
+                      width= 8,
+                      command=lambda: In_Lock(sage))
+    
+    Skye = tk.Button(root,
+                      text="Skye",
+                      height= 3,
+                      width= 8,
+                      command=lambda: In_Lock(skye))
+    
+    Sova = tk.Button(root,
+                      text="Sova",
+                      height= 3,
+                      width= 8,
+                      command=lambda: In_Lock(sova))
+    
+    Viper = tk.Button(root,
+                      text="Viper",
+                      height= 3,
+                      width= 8,
+                      command=lambda: In_Lock(viper))
+    
+    Yoru = tk.Button(root,
+                      text="Yoru",
+                      height= 3,
+                      width= 8,
+                      command=lambda: In_Lock(yoru))
+    
+    
+    Astra.grid(row= 1,column= 0, padx= 5, pady= 6)
+    
+    Breach.grid(row= 1,column= 1, padx= 6, pady= 6)
+    
+    Brimstone.grid(row= 1,column= 2, padx= 6, pady= 6)
+    
+    Chamber.grid(row= 1,column= 3, padx= 6, pady= 6)
+    
+    Clove.grid(row= 1,column= 4, padx= 6, pady= 6)
+    
+    Cypher.grid(row= 1,column= 5, padx= 6, pady= 6)
+    
+    Deadlock.grid(row= 1,column= 6, padx= 6, pady= 6)
+    
+    Fade.grid(row= 1,column= 7, padx= 6, pady= 6)
+    
+    Gekko.grid(row= 1,column= 8, padx= 6, pady= 6)
+    
+    Harbor.grid(row= 2, column= 0, padx= 5, pady= 6)
+    
+    Iso.grid(row= 2, column= 1, padx= 6, pady= 6)
+    
+    Jett.grid(row= 2, column= 2, padx= 6, pady= 6)
+    
+    Kayo.grid(row= 2, column= 3, padx= 6, pady= 6)
+    
+    Killjoy.grid(row= 2, column= 4, padx= 6, pady= 6)
+    
+    Neon.grid(row= 2, column= 5, padx= 6, pady= 6)
+    
+    Omen.grid(row= 2, column= 6, padx= 6, pady= 6)
+    
+    Phoenix.grid(row= 2, column= 7, padx= 6, pady= 6)
+    
+    Raze.grid(row= 2, column= 8, padx= 6, pady= 6)
+    
+    Reyna.grid(row= 3, column= 0, padx= 5, pady= 6)
+    
+    Sage.grid(row= 3, column= 1, padx= 6, pady= 6)
+    
+    Skye.grid(row= 3, column= 2, padx= 6, pady= 6)
+    
+    Sova.grid(row= 3, column= 3, padx= 6, pady= 6)
+    
+    Viper.grid(row= 3, column= 4, padx= 6, pady= 6)
+    
+    Yoru.grid(row= 3, column= 5, padx= 6, pady= 6)
 
 def main_window():
     clear_screen()
